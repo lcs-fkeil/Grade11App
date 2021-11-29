d9dc30204164f81323f8752564e427c8c2219960
Franka Keil <fkeil@lcs.on.ca>
2021-11-29 14:25:42 -0500

    Initial Commit

---
+++ LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Franka Keil on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
